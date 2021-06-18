d1603babc377e6855e19ab7768a629de3717c7b0
Mike Laramie <mike.laramie@lacework.net>
2021-06-18 00:18:58 -0400

    GCP:  Troubleshooting audit log

--- gcp/gcp.tf
+++ gcp/gcp.tf
@@ -12,7 +12,7 @@ module "gcp_organization_audit_log" {
 
   bucket_force_destroy         = true
   org_integration              = true
-  use_existing_service_account = false
+  //use_existing_service_account = true
   //service_account_name         = module.gcp_organization_config.service_account_name
   //service_account_private_key  = module.gcp_organization_config.service_account_private_key
   organization_id              = var.gcp_org_id
