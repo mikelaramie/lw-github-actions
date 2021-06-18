ec5862ef74284106f1d7bff26ddeaa5b3e4a0359
Mike Laramie <mike.laramie@lacework.net>
2021-06-18 00:09:09 -0400

    GCP: removing config integration

--- gcp/gcp.tf
+++ gcp/gcp.tf
@@ -1,17 +1,19 @@
+/* Deleting until ALLY-545 is resolved
 module "gcp_organization_config" {
   source = "lacework/config/gcp"
 
   org_integration = true
   organization_id = var.gcp_org_id
 }
+*/
 
 module "gcp_organization_audit_log" {
   source = "lacework/audit-log/gcp"
 
   bucket_force_destroy         = true
   org_integration              = true
-  use_existing_service_account = true
-  service_account_name         = module.gcp_organization_config.service_account_name
-  service_account_private_key  = module.gcp_organization_config.service_account_private_key
+  use_existing_service_account = false
+  //service_account_name         = module.gcp_organization_config.service_account_name
+  //service_account_private_key  = module.gcp_organization_config.service_account_private_key
   organization_id              = var.gcp_org_id
 }
