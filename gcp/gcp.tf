2be32b3362494b7e9799bc7ec032fdf82de037e7
Mike Laramie <mike.laramie@lacework.net>
2021-06-19 15:38:01 -0400

    gcp: troubleshooting audit integration

--- gcp/gcp.tf
+++ gcp/gcp.tf
@@ -10,18 +10,18 @@ module "gcp_organization_config" {
 module "gcp_organization_audit_log" {
   source = "lacework/audit-log/gcp"
 
-  bucket_force_destroy         = true
-  org_integration              = true
+  bucket_force_destroy = true
+  org_integration      = true
   //use_existing_service_account = true
   //service_account_name         = module.gcp_organization_config.service_account_name
   //service_account_private_key  = module.gcp_organization_config.service_account_private_key
-  organization_id              = var.gcp_org_id
+  organization_id = var.gcp_org_id
 }
 
 // Adding IAM grant to allow for a clean destroy until v1.0.2 of audit-log/gcp is released
 
 resource "google_storage_bucket_iam_member" "audit-sa" {
   bucket = module.gcp_organization_audit_log.bucket_name
-  role = "roles/storage.admin"
+  role   = "roles/storage.admin"
   member = "serviceAccount:${var.gcp_sa_email}"
 }
--- gcp/variables.tf
+++ gcp/variables.tf
@@ -13,16 +13,16 @@ variable "gcp_sa_email" {
 
 // Lacework
 variable "lw_account" {
-    description = "Lacework Account to integrate with"
-    type        = string
+  description = "Lacework Account to integrate with"
+  type        = string
 }
 variable "lw_api_key" {
-    description = "API Key to use for Lacework integrations"
-    type        = string
-    sensitive   = true
+  description = "API Key to use for Lacework integrations"
+  type        = string
+  sensitive   = true
 }
 variable "lw_api_secret" {
-    description = "API Secret for lacework_api_key"
-    type        = string
-    sensitive   = true
+  description = "API Secret for lacework_api_key"
+  type        = string
+  sensitive   = true
 }
